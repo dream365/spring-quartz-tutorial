2f3cad60633ff5c5f9d45d7bdb93a45348bfa3c9
dream365 <kosewon_@naver.com>
2020-04-07 15:16:34 +0900

    Add job detail about sample job

---
+++ src/main/kotlin/com/dream365/springquartz/scheduler/SpringQuartzScheduler.kt
@@ -0,0 +1,30 @@
+package com.dream365.springquartz.scheduler
+
+import com.ybrain.klar.backend.logging.ILogging
+import com.ybrain.klar.backend.logging.LoggingImpl
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.quartz.JobDetailFactoryBean
+
+
+//@see <a href="https://www.baeldung.com/spring-quartz-schedule">Scheduling in Spring with Quartz</a>
+@Configuration
+class SpringQuartzScheduler : ILogging by LoggingImpl<SpringQuartzScheduler>() {
+
+    /*
+    While the job is the workhorse, Quartz does not store an actual instance of the job class.
+    Instead, we can define an instance of the Job using the JobDetail class.
+    The job's class must be provided to the JobDetail so that it knows the type of the job to be executed.
+    */
+    @Bean
+    fun jobDetail(): JobDetailFactoryBean {
+        val jobDetailFactory = JobDetailFactoryBean()
+        jobDetailFactory.setJobClass(SampleJob::class.java)
+        jobDetailFactory.setName("Sample_Qrtz_Job_Detail")
+        jobDetailFactory.setDescription("Invoke Sample Job service...")
+        jobDetailFactory.setDurability(true)
+        return jobDetailFactory
+    }
+
+
+}
