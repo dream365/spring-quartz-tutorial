efba8befffde3883d969105840f7ea880bcefbb7
dream365 <kosewon_@naver.com>
2020-04-07 13:37:17 +0900

    Add spring main application

---
+++ src/main/kotlin/com/dream365/springquartz/SpringQuartzApplication.kt
@@ -0,0 +1,15 @@
+package com.dream365.springquartz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@SpringBootApplication
+@EnableReactiveMongoRepositories
+@EnableScheduling
+class SpringQuartzApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringQuartzApplication>(*args)
+}
