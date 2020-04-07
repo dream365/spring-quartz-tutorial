42b9ffeb2b1f03471eee91e3c1d300f171ddc67d
dream365 <kosewon_@naver.com>
2020-04-07 16:41:15 +0900

    Add quartz cron and simple trigger

--- src/main/kotlin/com/dream365/springquartz/scheduler/SpringQuartzScheduler.kt
+++ src/main/kotlin/com/dream365/springquartz/scheduler/SpringQuartzScheduler.kt
@@ -1,12 +1,19 @@
 package com.dream365.springquartz.scheduler
 
+import com.dream365.springquartz.config.AutoWiringSpringBeanJobFactory
 import com.ybrain.klar.backend.logging.ILogging
 import com.ybrain.klar.backend.logging.LoggingImpl
-import org.quartz.JobBuilder
 import org.quartz.JobDetail
+import org.quartz.Trigger
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean
 import org.springframework.scheduling.quartz.JobDetailFactoryBean
+import org.springframework.scheduling.quartz.SchedulerFactoryBean
+import org.springframework.scheduling.quartz.SpringBeanJobFactory
+import java.util.*
 
 
 /** @see <a href="https://www.baeldung.com/spring-quartz-schedule">Scheduling in Spring with Quartz</a> **/
@@ -39,5 +46,33 @@ class SpringQuartzScheduler : ILogging by LoggingImpl<SpringQuartzScheduler>() {
             .build()
     }*/
 
+    /**
+     * A Trigger is the mechanism to schedule a Job, i.e. a Trigger instance “fires” the execution of a job.
+     * There's a clear separation of responsibilities between the Job (notion of task) and Trigger (scheduling mechanism).
+     **/
+    @Bean
+    fun cronTrigger(job: JobDetail): CronTriggerFactoryBean {
+        val trigger = CronTriggerFactoryBean()
+        trigger.setJobDetail(job)
+
+        log.info("Configuring cron trigger to fire every 5 seconds")
+        trigger.setCronExpression("0/5 * * 1/1 * ? *")
+        trigger.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"))
+        trigger.setName("Qrtz_Cron_Trigger")
+        return trigger
+    }
+
+    /*
+    @Bean
+    fun simpleTrigger(job: JobDetail?): SimpleTriggerFactoryBean? {
+        val trigger = SimpleTriggerFactoryBean()
+        trigger.setJobDetail(job)
+        val frequencyInSec = 10
+        log.info("Configuring trigger to fire every {} seconds", frequencyInSec)
+        trigger.setRepeatInterval(frequencyInSec * 1000.toLong())
+        trigger.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY)
+        trigger.setName("Qrtz_Simple_Trigger")
+        return trigger
+    }*/
 
 }
